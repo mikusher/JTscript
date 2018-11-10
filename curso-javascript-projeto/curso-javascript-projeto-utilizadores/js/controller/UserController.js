a23ea84728f599d673671debaa1367a1bdea122f
mikusher <mikusher@hotmail.com>
2018-11-10 17:15:09 +0000

    Excluir utilizador

--- curso-javascript-projeto/curso-javascript-projeto-utilizadores/js/controller/UserController.js
+++ curso-javascript-projeto/curso-javascript-projeto-utilizadores/js/controller/UserController.js
@@ -43,7 +43,7 @@ class UserController {
                         <td>${Utils.dateFormat(result._register)}</td>
                         <td>
                           <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                          <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                          <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
                         </td>
                     `;
 
@@ -170,7 +170,7 @@ class UserController {
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
               <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-              <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+              <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
             </td>
         `;
 
@@ -207,6 +207,15 @@ class UserController {
     }
 
     addEventsTr(tr) {
+
+        tr.querySelector(".btn-delete").addEventListener("click", e=>{
+            if(confirm("Deseja realmente excluir?")){
+                tr.remove();
+                this.updateCount();
+            }
+
+        });
+
         tr.querySelector(".btn-edit").addEventListener("click", e=>{
             let json = JSON.parse(tr.dataset.user);
             this.formUP.dataset.trIndex = tr.sectionRowIndex;
